4fe0659f497fc12169ff665cca207e986bf7910b
addin <addin@addin-K43SJ>
2014-03-14 23:47:15 +0700

    adding documentation in category controller and putting some conf of pagination in config folder.

---
+++ application/config/pagination.php
@@ -0,0 +1,30 @@
+<?php if (!defined('BASEPATH'))exit('No direct script access allowed');
+/*
+  | -------------------------------------------------------------------------
+  | Pagination
+  | -------------------------------------------------------------------------
+  | I create this file to configure pagination style using Zurb Foundation 5.1.1
+  |
+ */
+
+$config['full_tag_open'] = '<div class="pagination-centered"><ul class="pagination">';
+$config['full_tag_close'] = '</ul></div>';
+$config['first_link'] = '&inodot;&blacktriangleleft;';
+$config['first_tag_open'] = '<li class="arrow">';
+$config['first_tag_close'] = '</li>';
+$config['last_link'] = '&blacktriangleright;&inodot;';
+$config['last_tag_open'] = '<li class="arrow">';
+$config['last_tag_close'] = '</li>';
+$config['next_link'] = '&blacktriangleright;';
+$config['next_tag_open'] = '<li class="arrow">';
+$config['next_tag_close'] = '</li>';
+$config['prev_link'] = '&blacktriangleleft;';
+$config['prev_tag_open'] = '<li class="arrow">';
+$config['prev_tag_close'] = '</li>';
+$config['cur_tag_open'] = '<li class="current"><a>';
+$config['cur_tag_close'] = '</a></li>';
+$config['num_tag_open'] = '<li>';
+$config['num_tag_close'] = '</li>';
+
+/* End of file pagination.php */
+/* Location: ./application/config/pagination.php */
